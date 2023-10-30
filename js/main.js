48b7b75cc4a11cab6f31820fdd419033f8eb69bb
Erbosyn Nurbol <ErbosynNurbol@gmail.com>
2023-10-30 18:50:32 +0600

    33

---
+++ js/main.js
@@ -0,0 +1,67 @@
+//Number Booleen String Object Undefined Null Symbol
+// + - * / % 
+
+let width = 100;
+let height = 120;
+
+let p = (width+ height)*2;
+let s = width * height;
+let s2 = width*height/2;
+
+let r = 100;
+const pi = 3.14;
+let s3 = pi*r*r;
+
+let weight = 5;//kg
+let price = 4.5; //$ 15000 * 0.1
+let totalPrice = weight * price;
+
+let num1 = 0.1; //0000110101
+let num2 = 0.2; //0001010101
+let sum = (num1*100 + num2*100)/100; //float
+
+let num3 = Number.MAX_VALUE * 345324535;
+if(num3 == Infinity){
+
+}
+let num4 = Number.MAX_SAFE_INTEGER;
+let articleContent = ``;
+
+
+let myNull = null;
+let booNull = Boolean(myNull);
+let numNull = parseInt("Erlan"); //NaN =>Not a Number
+
+/*
+let text = `<h1>
+        <a href="https://elroda.com">Elorda IT \`sdf\` </a>
+</h1>`;
+*/
+
+let num5  = 100;
+// /*
+//     num5 += 40; //num5  = num5 + 40;
+//     num5 -= 30; //num5 = num5 - 30;
+// */
+//     num5 %= 5; //num5 = num5 % 5;
+//     num5++; //num5 = num5 +1;
+//     ++num5;
+
+// console.log(--num5);
+// console.log(num5);
+let num7  = 120;
+let num6  = !true;
+
+let num9 = (!((!true ? 10 : -10) < -10)?10:-10)?10:-10; //
+if(!true)//Egeege hdffg 
+{
+
+}else{ //
+
+}
+console.log(num9);
+
+
+
+
+
