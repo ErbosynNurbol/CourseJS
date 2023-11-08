805371994ea8f358da9c8100a72a504083d5c974
Erbosyn Nurbol <ErbosynNurbol@gmail.com>
2023-11-08 11:56:55 +0600

    sss

--- js/main.js
+++ js/main.js
@@ -1,69 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const myDiv = document.querySelector(".myDiv");
+  myDiv.addEventListener("mouseover", () => {
+    myDiv.style = "background-color:#0094ff;";
+  });
+  myDiv.addEventListener("mouseout", () => {
+    myDiv.style = "background-color:red;";
+  });
+  myDiv.addEventListener("click", () => {
+    alert("Salem!!!");
+  });
+  const btn = myDiv.querySelector("button");
+  btn.addEventListener("click", (ev) => {
+    window.location.href = "https://elorda.com";
+    ev.stopPropagation();
+  });
 
-// let h1Arr = document.getElementsByClassName('title'); //document.getElementsByTagName('h1');
-// for(let index in h1Arr){
-//   h1Arr[index].innerText = "Salem Alem!!";
-// }
-// console.log(h1Arr);
+  const links = document.querySelectorAll("a");
+  links.forEach((el) => {
+    el.addEventListener("click", (ev) => {
+      window.location.href = "ht"+"tps://an"+"kui.kz";
+      ev.preventDefault();
+      ev.stopPropagation();
+    });
+  });
 
-// let discraption = document.getElementById('discraption');
-// discraption.style = "font-size:25px";
-// discraption.innerText = "hello";
-// discraption.innerHTML = '<a href="https://elorda.com">Elorda IT</a>';
-
-// let discraption =  document.querySelector('.title');
-
-// let subTitles =  document.querySelectorAll('h1');
-// for(var item of subTitles){
-//   item.innerHTML = 'Wa hhahaahhaha';
-// }
-
-
-// let names = new Array('Nurbol','Erlan','Asyl','Talant','Jhon');
-// let ul = document.querySelector('#student-list');
-// for(let name of names){
-//   let li = document.createElement('li');
-//   li.innerText = name;
-//   ul.appendChild(li);
-// }
-
-
-const getWords = async ()=>{
-  const response = await fetch('https://www.sozdikqor.org/api/query/all',
-  {"method":'post'});
-  const words = await response.json();
-
-  let tbody = document.querySelector('.word-list tbody'); //Html element
-  let fragment = new DocumentFragment();
-  let trIndex = 0; //Number
-  for(let key in words){
-    let tr = document.createElement('tr'); //Html Element
-    //Index #
-    let tdIndex  = document.createElement('td');
-    tdIndex.innerText = ++trIndex;
-    tr.appendChild(tdIndex);
-    //local Key
-    let td  = document.createElement('td');
-    td.innerText = key;
-    tr.appendChild(td);
-    //local value
-
-    let innerJson = words[key];
-
-    for(let innerKey in innerJson){
-      let innerTd  = document.createElement('td');
-      innerTd.innerText = words[key][innerKey];
-      tr.appendChild(innerTd);
-    }
-    // Manage
-     let tdManage = document.createElement('td');
-     tdManage.innerHTML = '<button>View</button>';
-     tr.appendChild(tdManage);
-    //tbody
-    fragment.appendChild(tr);
-  }
-  tbody.appendChild(fragment);
-  const loadingDiv = document.querySelector('#loading');
-  loadingDiv.style = 'display:none;';
-};
-
-document.addEventListener('DOMContentLoaded',getWords);
+  const txt = document.querySelector('#txt');
+  txt.addEventListener('keypress',(ev) => {
+      if(ev.keyCode == 65){
+       let content = txt.value;
+       const start = txt.selectionStart;//index
+       let newText = content.substring(0,start);
+       newText += '@';
+       newText += content.substring(start);
+        txt.value = newText;
+        txt.selectionStart = txt.selectionEnd = start + 1;
+        ev.preventDefault();
+     
+      }else  if(ev.keyCode == 97){
+        alert('a');
+        ev.preventDefault();
+      }
+  });
+});
